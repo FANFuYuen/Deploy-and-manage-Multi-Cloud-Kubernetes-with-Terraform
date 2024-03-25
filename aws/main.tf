351e713d88b1877472d14a14a977d0ff35a45151
Kyle FAN <220073549@stu.vtc.edu.hk>
2024-03-25 16:01:29 +0800

    update aws main.tf

--- aws/main.tf
+++ aws/main.tf
@@ -48,7 +48,7 @@ module "vpc" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "19.15.3"
+  version = "20.8.4"
 
   cluster_name    = local.cluster_name
   cluster_version = "1.27"
