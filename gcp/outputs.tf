3b2d9f6a907f8a7f1b48b942e4fed78301873212
Kyle FAN <220073549@stu.vtc.edu.hk>
2024-04-24 21:28:19 +0800

    update gcp

--- gcp/outputs.tf
+++ gcp/outputs.tf
@@ -1,11 +1,30 @@
-output "cluster_endpoint" {
-  value = google_container_cluster.primary.endpoint
+variable "project_id" {
+  description = "GCP project_id"
+  default     = "fan-itp4121mcloud"
 }
 
-output "project_id" {
-    value = var.project_id
+variable "credentials_file" {
+    default = ["..//key/gcloud_service_account.json"]
+ }
+
+variable "region" {
+  default     = "asia-east2"
+  description = "GCP Region"
 }
 
-output "region" {
-    value = var.region
+variable "zone"{
+  description = "Zone for infrastructure"
+  default = "asia-east2-a"
+}
+
+variable "private_cidr_ranges" {
+  type          = list(string)
+  description   = "Range of private subnetwork."
+  default       = ["10.10.1.0/24", "10.10.2.0/24"]
+}
+
+variable "public_cidr_ranges" {
+  type          = list(string)
+  description   = "Range of public subnetwork."
+  default       = ["10.10.3.0/24", "10.10.4.0/24"]
 }
